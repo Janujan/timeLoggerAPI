5c3fdffc38b926a3e59a9cf7834f4b96b2d968d4
Janujan <janujan.selva@gmail.com>
2020-04-02 17:11:26 -0400

    change endpoint output to response entities with correesponding http status code

--- timelog/src/main/java/com/timelog/timelog/controllers/LogController.java
+++ timelog/src/main/java/com/timelog/timelog/controllers/LogController.java
@@ -2,10 +2,9 @@ package com.timelog.timelog.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
 
 import javax.validation.ConstraintViolationException;
 
@@ -15,6 +14,8 @@ import com.timelog.timelog.models.User;
 import com.timelog.timelog.models.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,21 +31,13 @@ public class LogController {
     @Autowired
     private UserRepository userRepository;
 
-    // @ResponseStatus(value = HttpStatus.OK, reason = "")
     //This function will now get user based on user id passed in via path variable
     //Then it will add the log under that user.
-    @RequestMapping(value="/logs/submit/{username}", method=RequestMethod.POST)
-    public HashMap<String,String> submitLog(@PathVariable final String username, @RequestBody final Log submitData){
+    @RequestMapping(value="/logs/submit", method=RequestMethod.POST)
+    public ResponseEntity<?> submitLog(Principal principal, @RequestBody final Log submitData){
 
-        final HashMap<String, String> response = new HashMap<String,String>();
 
-        final User qUser = userRepository.findByUsername(username);
-    
-
-        if(qUser == null){
-            response.put("error", "user doesnt exist, register first");
-            return response;
-        }
+        final User qUser = userRepository.findByUsername(principal.getName());
 
         submitData.setUser(qUser);
         System.out.println(submitData.toString());
@@ -53,21 +46,24 @@ public class LogController {
             logRepository.save(submitData);
         }
         catch(final ConstraintViolationException e){
-            response.put("error", "missing required paramater(s)");
+            System.out.println("log not saved");
+            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
         }
-
-        response.put("success!", "log saved");
-        return response;
+        
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 
-    @RequestMapping(value="/logs/{username}/{logid}", method=RequestMethod.GET)
-    public Optional<Log> getLogbyID(@PathVariable final long logid){
-        return logRepository.findById(logid);
+    @RequestMapping(value="/logs/{logid}", method=RequestMethod.GET)
+    public Log getLogbyID(Principal principal, @PathVariable final long logid){
+        User qUser = userRepository.findByUsername(principal.getName());
+
+        Log log = logRepository.findLogByUser(qUser.getId(), logid);
+        return log;
     }
 
-    @RequestMapping(value="/logs/{username}", method=RequestMethod.GET)
-    public Iterable<Log> getLogs(@PathVariable final String username){
-        final User qUser = userRepository.findByUsername(username);
+    @RequestMapping(value="/logs/", method=RequestMethod.GET)
+    public Iterable<Log> getLogs(Principal principal){
+        final User qUser = userRepository.findByUsername(principal.getName());
         List<Log> result = new ArrayList<Log>();
         if(qUser ==  null){
             return result;
@@ -75,26 +71,20 @@ public class LogController {
         result = logRepository.findAllLogsByUser(qUser.getId());
 
         return result;
-
     }
 
     // Create custom query for getting logs from a specific user
-    @RequestMapping(value="/logs/{username}/{logid}", method=RequestMethod.POST)
-    public HashMap<String, String> updateLogById(@PathVariable final String username, @PathVariable final long logid, @RequestBody final Log updateData){
-        final HashMap<String,String> response =  new HashMap<String, String>();
-        //logic is to find the id, then replace with new data
-
-        //Find USer
-        final User qUser = userRepository.findByUsername(username);
-        if(qUser ==  null){
-            response.put("error", "user doesnt exist, please register first");
-            return response;
-        }
+    @RequestMapping(value="/logs/{logid}", method=RequestMethod.POST)
+    public ResponseEntity<?> updateLogById(Principal principal, @PathVariable final long logid, @RequestBody final Log updateData){
+        //get User
+        final User qUser = userRepository.findByUsername(principal.getName());
 
         //Find logs by the user if found
         final Log oldData = logRepository.findLogByUser(qUser.getId(), logid);
+        
         if(oldData == null){
-            response.put("error", "log id doesnt exist");
+            System.out.println("not found");
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         else{
 
@@ -102,40 +92,29 @@ public class LogController {
             if(!updateData.equals(oldData)){
 
                 updateData.setid(oldData.getid());
+                updateData.setUser(qUser);
                 logRepository.save(updateData);
-                response.put("success", "updated log");
+            }
 
-            }
-            else{
-                response.put("unchanged","no new data was provided");
-            }
         }
-        return response;
-
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     //this uses the find first and then delete approach
-    @RequestMapping(value="/logs/delete/{username}/{logid}", method=RequestMethod.DELETE)
-    public HashMap<String, String> deleteLogbyID(@PathVariable final String username, @PathVariable final long logid){
-        final HashMap<String,String> response = new HashMap<String, String>();
+    @RequestMapping(value="/logs/delete/{logid}", method=RequestMethod.DELETE)
+    public ResponseEntity<?> deleteLogbyID(Principal principal, @PathVariable final long logid){
 
-        final User qUser = userRepository.findByUsername(username);
-        if(qUser ==  null){
-            response.put("error", "user doesnt exist, please register first");
-            return response;
-        }
+        final User qUser = userRepository.findByUsername(principal.getName());
 
         final Log result = logRepository.findLogByUser(qUser.getId(), logid);
         if(result == null){
-            response.put("nothing changed", "log id doesnt exist");
+            return new ResponseEntity<>(HttpStatus.OK);
         }
         else{
             logRepository.deleteById(logid);
-            response.put("success", "log was deleted");
-
         }
         
-        return response;
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
 
